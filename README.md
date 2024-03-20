393503810f1393d0fa394ac6149f4feb1d1bb2ec
Marco De Stefano <destmark@gmail.com>
2024-03-20 11:51:08 +0100

    updated index.js

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # askgemini
+
+API wrapper for Gemini Pro https://ai.google.dev/tutorials/node_quickstart
+
+API available on : https://askgemini.onrender.com/api
+
+endpoint:
+text (POST) - with Content-Type application/json 
+
+input parameters:
+```
+apiKey:         your Gemini API Key - get one from https://makersuite.google.com/app/apikey
+textPrompt:     your text prompt
+```
+
+output:
+a json containing the message answer from Gemini
+
+sample input:
+```
+{
+    "apiKey": "myApiKey",
+    "textPrompt": "What's your name?"
+}
+```
+
+sample output:
+```
+{
+    "message": "I am Gemini, a multimodal AI language model developed by Google.
+}
+```
---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8888;
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+app.post('/api/text', async (req, res) => {
+    if (req.method === 'POST' && req.headers['content-type'] === 'application/json') {
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk;
+        });
+        req.on('end', () => {
+            try {
+                const jsonData = JSON.parse(data);
+                const apiKey = jsonData.apiKey;
+                const textPrompt = jsonData.textPrompt;
+                if (apiKey && textPrompt) {
+                    const genAI = new GoogleGenerativeAI(apiKey);
+                    const model = genAI.getGenerativeModel({
+                        model: "gemini-pro", "safetySettings": [
+                            {
+                                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                                "threshold": "BLOCK_NONE"
+                            },
+                            {
+                                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                                "threshold": "BLOCK_NONE"
+                            },
+                            {
+                                "category": "HARM_CATEGORY_HATE_SPEECH",
+                                "threshold": "BLOCK_NONE"
+                            },
+                            {
+                                "category": "HARM_CATEGORY_HARASSMENT",
+                                "threshold": "BLOCK_NONE"
+                            }
+                        ]
+                    });
+                    const result = model.generateContent(textPrompt).then(result => {
+                        return result.response.text();
+                    }).then(text => {
+                        res.writeHead(200, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify({ message: text }));
+                    })
+                }
+                else {
+                    res.writeHead(400, { 'Content-Type': 'text/plain' });
+                    res.end('Invalid JSON data');
+                }
+            } catch (error) {
+                res.writeHead(400, { 'Content-Type': 'text/plain' });
+                res.end('Invalid JSON data');
+            }
+        });
+    } else {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Unsupported method or content type');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+
